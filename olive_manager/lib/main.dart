2479d395e6e21c1671e1694dd7e2d1f62596855a
JohnBarbounis <csd4468@csd.uoc.gr>
2026-03-23 11:57:45 +0200

    internet access

--- olive_manager/lib/main.dart
+++ olive_manager/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart'; // Θα το φτιάξουμε αμέσως μετά!
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const OliveManagerApp());
